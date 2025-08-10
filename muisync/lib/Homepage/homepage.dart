09f93c2590066133ef7b6107c996f7d5a473d2f5
AshiqMohammed-M <ashiqmohammed0055@gmail.com>
2025-08-10 14:55:45 +0530

    creating a home page for the app, including join room and create room button and an json animation

--- muisync/lib/Homepage/homepage.dart
+++ muisync/lib/Homepage/homepage.dart
@@ -1,17 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text(
+          "MuiSync",
+          style: GoogleFonts.roboto(
+            textStyle: TextStyle(color: Colors.black),
+          ),
+        ),
+      ),
+
+      body: Stack(
+        children: [
+          // Join Room Button - Top Left
+          Positioned(
+            top: screenHeight * 0.5,
+            left: screenWidth * 0.5,
+            child: ElevatedButton(
+              onPressed: () {
+                // TODO: Join room logic
+              },
+              child: const Text("Join Room"),
+            ),
+          ),
+
+          // Create Room Button - Top Right
+          Positioned(
+            top: screenHeight * 0.05,
+            right: screenWidth * 0.05,
+            child: ElevatedButton(
+              onPressed: () {
+                // TODO: Create room logic
+              },
+              child: const Text("Create Room"),
+            ),
+          ),
+
+          // Lottie Animation in Center
+          Center(
+            child: SizedBox(
+              width: screenWidth * 0.6,
+              height: screenHeight * 0.4,
+              child: Lottie.asset(
+                'assets/astronaut_music.lottie', // Replace with your animation file
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- muisync/lib/main.dart
+++ muisync/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:muisync/Homepage/homepage.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
